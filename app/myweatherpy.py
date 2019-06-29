d87303f5408b1bab66aeff8d733315a3d4241223
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-28 20:54:58 -0400

    Update for displaying refresh time, weather condition and other information.

--- app/myweatherpy.py
+++ app/myweatherpy.py
@@ -5,13 +5,20 @@ import requests
 import json
 import re
 import datetime
+import tzlocal
 
 from dotenv import load_dotenv
+from datetime import datetime
+
 
 load_dotenv()
 
 API_KEY =os.environ.get("openweather_API") # to obtain API_KEY from env file. 
 API_KEY2 =os.environ.get("darksky_api")
+
+current_time = datetime.now()  #> current time
+#formatted_current_time = current_time.strftime("%Y-%m-%d %H:%M:%S")  #>'2019-06-21 14:00:00' (reference: from prior class discussion)
+
 #
 #
 #
@@ -21,9 +28,10 @@ API_KEY2 =os.environ.get("darksky_api")
 print("Welcome to MyWeatherPy. We are here to help you to provide information\n"
             "about your curiosity in the weather condition anywhere")
 
+# Ask User Input
 confirmation = input("Are you ready to explore MyWeatherPy? Please press n to exit.\n"
                 "Otherwise, please press any key if you would like to proceed:  ")
-#while True:
+
 if confirmation == "n":
     print("Thank you for considering, MyWeatherPy. We hope you visit us again in the future!")
     exit()
@@ -31,17 +39,60 @@ else:
     user_input = input("Awesome choice! Please enter zip code, or city name and state to start\n"
                         "For example, you can input 10004 or New York,NY: ")
     
-    #def get_response(user_input):  #> To define and return the result after user input. 
-    request_url = f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={API_KEY}"
-    response = requests.get(url = request_url)
+    # Request weather, condition, forecast etc using API credentials through HTML request
+    request_url_1 = f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={API_KEY}"
+    request_url_2 = f"https://api.openweathermap.org/data/2.5/weather?zip={user_input}&appid={API_KEY}"
+    response = requests.get(url = request_url_1 or request_url_2)
     
     if response.status_code in [200, 301, 302, 304]:
         data = response.json()
-        print(data)
-        last_refreshed = data["dt"]
+
+        # Set up a variable for current time at local time zone
+        import tzlocal
+        last_time = data["dt"]  # Reference: https://stackoverflow.com/questions/3682748/converting-unix-timestamp-string-to-readable-date        
+        unix_timestamp = float(last_time)    
+        local_timezone = tzlocal.get_localzone()  #get pytz timezone
+        local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
+        
+        last_timezone = data["timezone"]
+        unix_timezone = float(last_time)    
+        local_timezone2 = tzlocal.get_localzone()  #get pytz timezone
+        local_time2 = datetime.fromtimestamp(unix_timezone, local_timezone2)
+        print(f"{local_time.strftime('%Y-%m-%d %H:%M:%S')}" + "  " f"{(local_time2.strftime('%Z'))}")
+        
+        # Set up a variable for current weather condition
+        list_weather = data["weather"]   
+        for w in list_weather:
+            print(w["main"])
+        
+         # Set up a variable for current weather condition - more description
+        for w in list_weather:         
+            print(w["description"])     
+
+        # Set up a variable for current temp
+        last_refreshed_temp = data["main"]["temp"] 
+        
+        def toCelcius(last_refreshed_temp):         # Convert to Celcius
+            return int(last_refreshed_temp-273.15)
+    
+        def toFahrenheit(last_refreshed_temp):      # Convert to Fahrenheit
+            return int((last_refreshed_temp-273.15)*9/5+32)
+        
+        print(f"{toCelcius(last_refreshed_temp)}"+"C")  
+        print(f"{toFahrenheit(last_refreshed_temp)}"+"F")
+
+        # Set up a variable for current temp
+        last_refreshed_hum = data["main"]["humidity"] 
+        print(f"{(last_refreshed_hum)}"+"%")
 
         
-        
+
+
+
+
+
+        #breakpoint()
+        #print(last_refreshed_time + "    " + current_weather)
         
         
         
@@ -179,10 +230,10 @@ else:
     # Decide whether we want to do command line style application or pop up screen, clickable type or HTML web application
 
 
-    # Ask User Input
+    
 
 
-    # Request weather, condition, forecast etc using API credentials through HTML request
+    
 
     # Ask wheather user wants to see historical data or forecast
 
