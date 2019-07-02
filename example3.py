1df717e9f53ed320506898add6336392cb0be810
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-07-02 12:01:17 -0400

    cleaning up the directory

--- example3.py
+++
@@ -1,6 +0,0 @@
-#import tzlocal issue
-#import sendgrid issue
-#import sendgrid.helpeers.mail issue
-#import dotenv issue
-#import selenium issueimport selenium.webdirver.common.keys issue
-#import tzlocal issue
